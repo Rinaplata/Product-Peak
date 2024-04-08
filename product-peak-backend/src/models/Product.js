70a7a1e7e0f8c4ea0c1fa10df6dff484a5fb0991
eldelahoz <andelahoz13@gmail.com>
2024-04-08 03:49:28 +0000

    ADD: Creacion del modelo product

---
+++ product-peak-backend/src/models/Product.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const ProductSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: String,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = model("Product", ProductSchema);
