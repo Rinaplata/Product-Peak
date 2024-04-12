9f2680a7ee3f68464a3db0737f4342faa6685551
eldelahoz <andelahoz13@gmail.com>
2024-04-12 00:16:14 +0000

    ADD: Se separa validacion de user de routes

--- product-peak-backend/src/routes/UserRoutes.js
+++ product-peak-backend/src/routes/UserRoutes.js
@@ -1,37 +1,24 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
-const { createUser, loginUser, renewToken } = require('../controllers/UserController');
-const { check } = require('express-validator');
-const { validateFields } = require('../middlewares/validateFields');
-
+const {
+  createUser,
+  loginUser,
+  renewToken,
+} = require("../controllers/UserController");
+const {
+  signUpValidator,
+  logInValidator,
+} = require("../validators/UserValidator");
+const { validateFields } = require("../middlewares/validateFields");
 
 // Create a new user
-router.post(
-  '/signUp', 
-  [
-    check('username', 'Username is mandatory').not().isEmpty(),
-    check('email', 'Email is mandatory').isEmail(),
-    check('password', 'Password min length is 6').isLength({ min: 6 }),
-    check('bio', 'Bio min length is 1000').isLength({ max: 1000 }),
-    check('avatar', 'Avatar is mandatory').not().isEmpty(),
-    validateFields
-  ],
-  createUser
-);
+router.post("/signUp", signUpValidator(), createUser);
 
 // login
 
-router.post(
-  '/logIn', 
-  [
-    check('email', 'Email is mandatory').isEmail(),
-    check('password', 'Password is mandatory').not().isEmpty(),
-    validateFields
-  ],
-  loginUser
-);
+router.post("/logIn", [logInValidator(), validateFields], loginUser);
 
-router.get('/renew', renewToken);
+router.get("/renew", renewToken);
 
 module.exports = router;
