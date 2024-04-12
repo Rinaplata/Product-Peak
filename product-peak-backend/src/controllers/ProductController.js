b3c844b603bfdc83748f75ed6a155b4ce9f5bbb1
eldelahoz <andelahoz13@gmail.com>
2024-04-12 03:56:06 +0000

    ADD: Se agrega metodo de creacion de producto

--- product-peak-backend/src/controllers/ProductController.js
+++ product-peak-backend/src/controllers/ProductController.js
@@ -1,2 +1,39 @@
 const { response } = require("express");
 const Product = require("../models/Product");
+const User = require("../models/User");
+
+const createProduct = async (req, res = response) => {
+  const { name, description, url, tags } = req.body;
+  try {
+    const product = new Product({
+      name,
+      description,
+      url,
+      tags: tags,
+      userId: res.userId,
+    });
+
+    await product.save();
+
+    return res.status(201).json({
+      ok: true,
+      error: {
+        message: "Product Registered",
+      },
+      product: {
+        id: product._id,
+        name: product.name,
+        description: product.description,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      error: {
+        message: "Something went worng, please contact to admin",
+      },
+    });
+  }
+};
+
+module.exports = { createProduct };
