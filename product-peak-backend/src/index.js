848ace26e63ad99e71072b0b4ae27857f25e000b
eldelahoz <andelahoz13@gmail.com>
2024-04-12 03:29:22 +0000

    ADD: Se agrega route de product

--- product-peak-backend/src/index.js
+++ product-peak-backend/src/index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const { connectDB } = require('./database/db');
-
+const express = require("express");
+const { connectDB } = require("./database/db");
 
 const app = express();
 
 // se usa para leer el archivo .env
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-// DB 
+// DB
 
 connectDB();
 
@@ -16,7 +15,9 @@ connectDB();
 app.use(express.json());
 
 // app routes
-app.use('/api/v1', require('./routes/UserRoutes') );
+app.use("/api/v1", require("./routes/UserRoutes"));
+// routes product
+app.use("/api/v1/products", require("./routes/ProductRoutes"));
 
 app.listen(process.env.PORT, () => {
   console.log(`SERVER RUNNING`);
