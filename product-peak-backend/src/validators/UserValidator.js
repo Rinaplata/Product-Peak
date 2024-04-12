5283ed779846bacedeb26fa144e5fa859614638e
eldelahoz <andelahoz13@gmail.com>
2024-04-12 03:28:51 +0000

    ADD: Se termina de agregar validator

--- product-peak-backend/src/validators/UserValidator.js
+++ product-peak-backend/src/validators/UserValidator.js
@@ -5,7 +5,7 @@ const signUpValidator = () => {
     body("username", "Username is mandatory").not().isEmpty(),
     body("email", "Email is mandatory").isEmail(),
     body("password", "Password min length is 6").isLength({ min: 6 }),
-    body("bio", "Bio max length is 1000").isLength({ max: 1000 }),
+    body("bio", "Bio min length is 1000").isLength({ max: 1000 }),
     body("avatar", "Avatar is mandatory").not().isEmpty(),
   ];
 };
