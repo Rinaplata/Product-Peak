ded64615146d47931616b482f2217cf63145abd1
eldelahoz <andelahoz13@gmail.com>
2024-04-13 03:45:32 +0000

    FIX: Se cambia orden ejecucion

--- product-peak-backend/src/swagger.js
+++ product-peak-backend/src/swagger.js
@@ -1,27 +1,21 @@
 const swaggerOptions = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Product Peak',
-            description: 'Product Peak es un clon de Product Hunt, la cual es una plataforma que permite a los usuarios descubrir y compartir nuevos productos y startups.',
-            contact: {
-                name: 'Rina Plata y Andres de la hoz'
-            },
-        },
-        servers: [
-            {
-                url: "http://localhost:3000/api/v1"
-            }
-        ],
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Product Peak",
+      description:
+        "Product Peak es un clon de Product Hunt, la cual es una plataforma que permite a los usuarios descubrir y compartir nuevos productos y startups.",
+      contact: {
+        name: "Rina Plata y Andres de la hoz",
+      },
     },
     apis: [
-        `${__dirname}/routes/UserRoutes.js`,
-        `${__dirname}/routes/ProductRoutes.js`,
-        `${__dirname}/routes/CommentRoutes.js`,
-        `${__dirname}/routes/RatingRoutes.js`
-    ]
-
-
-}
+      `${__dirname}/routes/UserRoutes.js`,
+      `${__dirname}/routes/ProductRoutes.js`,
+      `${__dirname}/routes/CommentRoutes.js`,
+      `${__dirname}/routes/RatingRoutes.js`,
+    ],
+  },
+};
 
 module.exports = swaggerOptions;
