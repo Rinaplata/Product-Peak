fb8c53ec6476553e5cd2a92f37982ed43bf53c81
eldelahoz <andelahoz13@gmail.com>
2024-04-13 04:10:55 +0000

    FIX: Separaciones en swagger

--- product-peak-backend/src/swagger.js
+++ product-peak-backend/src/swagger.js
@@ -8,19 +8,19 @@ const swaggerOptions = {
       contact: {
         name: "Rina Plata y Andres de la hoz",
       },
-      servers: [
-        {
-          url: "http://localhost:3000/api/v1",
-        },
-      ],
     },
-    apis: [
-      `${__dirname}/routes/UserRoutes.js`,
-      `${__dirname}/routes/ProductRoutes.js`,
-      `${__dirname}/routes/CommentRoutes.js`,
-      `${__dirname}/routes/RatingRoutes.js`,
+    servers: [
+      {
+        url: "http://localhost:3000/api/v1",
+      },
     ],
   },
+  apis: [
+    `${__dirname}/routes/UserRoutes.js`,
+    `${__dirname}/routes/ProductRoutes.js`,
+    `${__dirname}/routes/CommentRoutes.js`,
+    `${__dirname}/routes/RatingRoutes.js`,
+  ],
 };
 
 module.exports = swaggerOptions;
