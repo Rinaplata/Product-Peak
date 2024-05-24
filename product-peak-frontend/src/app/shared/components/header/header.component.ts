f5198ca78d0f72a19c286009c1ae3407f226d0df
rina plata <rinamarcelaplata@gmail.com>
2024-05-24 00:12:54 -0500

    Feat: interfaz register modal

--- product-peak-frontend/src/app/shared/components/header/header.component.ts
+++ product-peak-frontend/src/app/shared/components/header/header.component.ts
@@ -4,34 +4,52 @@ import { ButtonModule } from 'primeng/button';
 import { LoginComponent } from '../../../auth/login/login.component';
 import { DialogModule } from 'primeng/dialog';
 import { Dialog } from 'primeng/dialog';
+import { RegisterComponent } from '../../../auth/register/register.component';
 
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonModule, MenubarModule, LoginComponent, DialogModule],
+  imports: [ButtonModule, MenubarModule, LoginComponent, DialogModule, RegisterComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   @ViewChild('loginComponent') loginComponent!: LoginComponent;
+  @ViewChild('registerComponent') registerComponent!: RegisterComponent;
   visibleLogin: boolean = false;
+  visibleSignUp: boolean = false;
 
   loginSelected: any = {};
+  registerSelected: any = {}
 
   visible: boolean = false;
+  visibleModal: boolean = false;
 
   showDialog() {
       this.visible = true;
   }
 
+  showDialogRegister() {
+    this.visibleModal = true;
+}
+
   onCloseModalLogin(){
     this.visibleLogin = false;
+  }
+
+  onCloseModalRegister(){
+    this.visibleSignUp = false;
   }
 
   openModalLogin(loginSelected: any){
     this.loginSelected = loginSelected;
     this.visibleLogin = true;
+  }
+
+  openModalRegister(registerSelected: any){
+    this.registerSelected = registerSelected;
+    this.visibleSignUp = true;
   }
 }
